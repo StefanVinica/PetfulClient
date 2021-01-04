1c40de0fdb3b633646cae1f34668bfe83d7941f6
StefanVinica <stefanikolovski996@gmail.com>
2021-01-04 13:26:45 -0800

    fix

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
     REACT_APP_API_BASE_URL: 'https://hidden-fjord-73843.herokuapp.com',
-    API_BASE_URL: 'https://hidden-fjord-73843.herokuapp.com'
+    API_BASE_URL: 'https://hidden-fjord-73843.herokuapp.com',
   }
